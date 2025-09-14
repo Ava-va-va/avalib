d7232d5e452a2e1f2329c5d8d7fdbee68c331e54
Ava-va-va <3072508722@qq.com>
2025-09-14 20:41:46 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace ava1
+{
+    internal class Pro2
+    {
+        static void Main()
+        {
+            ////不同类型数据处理要转换成同一个类型，运算之后要存在同一类型变量中
+            //Console.WriteLine("请输入一个数字");
+            //string str = Console.ReadLine();
+            //string str1 = str + 20.ToString();
+            //Console.WriteLine(str);
+            //Console.WriteLine(str1);
+            //Console.ReadKey();
+            
+            //将字符串数组组合成一个路径
+            string[] paths = { @"d:\archives", "2001", "media", "images" };
+            string fullPath = Path.Combine(paths);
+            Console.WriteLine(fullPath);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ShowInfo.cs
@@ -0,0 +1,29 @@
+﻿using ava.ClassLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ava
+{
+    internal class ShowInfo
+    {
+        // .dll文件，发给别人之后，c#会对内部类型的访问，但是必须是public，访问类型要是对外设置访问
+        //public void ShowhrInfo(Hero hr)这里是静态的，只是执行一个简单的输出，不需要对实例化变量进行处理，可以用static的
+        public static void ShowhrInfo(Hero hr)
+        {
+            Console.WriteLine("性别：" + hr.Gender);
+            Console.WriteLine("年龄：" + hr.Age);
+            Console.WriteLine("基础伤害：" + hr.Damage);
+            Console.WriteLine("生命值：" + hr.HP);
+            Console.WriteLine("身高：" + hr.Tall);
+            Console.WriteLine("技能1：" + hr.Skill1);
+            Console.WriteLine("技能1伤害：" + hr.SkillDamage1);
+            Console.WriteLine("技能2：" + hr.Skill2);
+            Console.WriteLine("技能2伤害：" + hr.SkillDamage2);
+        }
+    }
+}
+       
+
